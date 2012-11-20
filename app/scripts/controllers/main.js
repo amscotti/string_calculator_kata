9c32855c2734b254050e1354024912fc45649b6d
Anthony Scotti <anthony.m.scotti@gmail.com>
2012-11-20 10:50:44 -0500

    20121120
    Notes:
    Got to number 4
    Put too much time in making my reduce function work but it did save me tons of time on number 3. I don't think this will be a issue next time around.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,13 @@
 'use strict';
 
 stringCalculatorKataApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+  $scope.add = function (numbers) {
+  	if(numbers.length > 0){
+		return numbers.split(/\n|,/).reduce(function(a,b){ 
+			return parseInt(a,10) + parseInt(b,10);
+		}, "0");
+  	} else {
+  		return 0;
+  	}	
+  }
 });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,32 @@ describe('Controller: MainCtrl', function() {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function() {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+  describe('add()', function() {
+    it('should return 0 for blank string', function() {
+      expect(scope.add("")).toBe(0);
+    });
+
+    it('should return 1 for "1"', function() {
+      expect(scope.add("1")).toBe(1);
+    });
+
+    it('should return 3 for "1,2"', function() {
+      expect(scope.add("1,2")).toBe(3);
+    });
+
+    it('should return 36 for "1,2,3,4,5,6,7,8"', function() {
+      expect(scope.add("1,2,3,4,5,6,7,8")).toBe(36);
+    });
+
+    it('should return 36 for "9,3,7,3,9,2,1"', function() {
+      expect(scope.add("9,3,7,3,9,2,1")).toBe(34);
+    });
+
+    it('should return 6 for "1\\n2,3"', function() {
+      expect(scope.add("1\n2,3")).toBe(6);
+    });
+
+
+  })
+
 });
