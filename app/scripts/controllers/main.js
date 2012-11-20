d712561019b2a46f3569e2e678ca7567ee879caf
Anthony Scotti <anthony.m.scotti@gmail.com>
2012-11-20 10:01:38 -0500

    Base project, ready to work on.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+stringCalculatorKataApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
