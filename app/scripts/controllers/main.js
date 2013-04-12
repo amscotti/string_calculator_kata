932528cc58a9e9a6d2bb6801d2bf73d2aa65d43e
Anthony Scotti <anthony.m.scotti@gmail.com>
2013-04-12 11:21:35 -0400

    looks like I forgot to commit this...oops

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,20 @@
 'use strict';
 
 stringCalculatorKataApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+  $scope.add = function (input) {
+  	if(input === ""){ return 0; }
+
+	var delimiter = /,|\n/;
+	if(input.match(/\/\/(.)\n/) !== null){
+		delimiter = input.match(/\/\/(.)\n/)[1]
+		input = input.replace(/\/\/(.)\n/, "")
+	}
+
+	var sum = input.split(delimiter).reduce(function(x, y) { 
+		return parseInt(x, 10) + parseInt(y, 10)
+	}, 0);
+
+	return sum;
+  };
+
 });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,30 @@ describe('Controller: MainCtrl', function() {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function() {
-    expect(scope.awesomeThings.length).toBe(3);
+  describe('Add()', function() {
+    it('empty string it should return 0', function() {
+      expect(scope.add("")).toBe(0);
+    });
+
+    it('a string of 1 number should return that number', function() {
+      expect(scope.add("1")).toBe(1);
+    });
+
+    it('a string of 2 number should be added', function() {
+      expect(scope.add("1,2")).toBe(3);
+    });
+
+    it('should handle an unknown amount of numbers', function() {
+      expect(scope.add("1,2,3,4,5,6,7,8,9")).toBe(45);
+    });
+
+    it('Add method should handle new lines between numbers', function() {
+      expect(scope.add("1\n2,3")).toBe(6);
+    });
+
+    it('Should be able to set delimiter', function() {
+      expect(scope.add("//;\n1;2")).toBe(3);
+    });
+
   });
 });
