97a76e76c58b6e29a972973fd680d21d0e06a8b2
Anthony Scotti <anthony.m.scotti@gmail.com>
2013-04-15 14:29:15 -0400

    20130415, off to a slow start

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,17 @@
 'use strict';
 
 stringCalculatorKataApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+  $scope.add = function(string_numbers) {
+  	string_numbers = string_numbers.length ? string_numbers : "0";
+	var match = (/\/\/(.)\n/).exec(string_numbers);
+	var delimiter = (match === null ? /,|\n/ : match[1]);
+	
+	if(match !== null){
+		string_numbers = string_numbers.replace(match[0], "");  			
+	}
+
+  	return string_numbers.split(delimiter).reduce(function(x,y) {
+  	 	return parseInt(x, 10) + parseInt(y, 10)
+  	}, 0);
+  };
 });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,35 @@ describe('Controller: MainCtrl', function() {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function() {
-    expect(scope.awesomeThings.length).toBe(3);
+   describe('add()', function() {
+
+    it('should return 0 for empty string', function() {
+       expect(scope.add("")).toBe(0);
+    });
+
+    it('should return a number as a int', function() {
+      expect(scope.add("1")).toBe(1);
+    });
+
+    it('should return sum of given numbers', function() {
+       expect(scope.add("1,2")).toBe(3);
+    });
+
+    it('should return sum of any unknown amount of numbers', function() {
+       expect(scope.add("1,2,3")).toBe(6);
+       expect(scope.add("1,2,3,4,6,7,8")).toBe(31);
+    });
+
+    it('should return sum when a newline is used instead of a commas', function() {
+       expect(scope.add("1\n2,3")).toBe(6);
+    });
+
+    it('should support different delimiters', function() {
+      expect(scope.add("//;\n1;2")).toBe(3);
+
+    });
+
+    
+
   });
 });
